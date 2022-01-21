b984003cee3c26c2bc872ce69d03c0ec88e9dfb9
Mike Salmon <m.salmon@uea.ac.uk>
2022-01-21 10:01:28 +0000

    section 5

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 class Site:
 
@@ -29,5 +30,10 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print('Not Implemented')
+            #print('Not Implemented')
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
 
